35a665c7eea9f99a62118cc92e36744ee5bcae54
Hiren Adesara <hiren.adesara@ge.com>
2014-10-16 11:58:18 -0700

    updated the require config iidx-brandkit

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -78,7 +78,7 @@ require.config({
         * To map IIDx to workbench, please modify path, map and shim section of this require js
         * The following section is copied from iidx require.config.js and path are re-mapped to bower_components/iids folder
         */
-        brandkit: '../bower_components/iids/dist/iidx/components/brandkit/js/brandkit',
+        brandkit: '../bower_components/iids/dist/iidx/components/brandkit/js/iidx-brandkit',
         'cascading-list': '../bower_components/iids/dist/iidx/components/cascading-list/js/cascading-list',
         charts: '../bower_components/iids/dist/iidx/components/charts/js/charts',
        'charts-theme': '../bower_components/iids/dist/iidx/components/charts/js/charts/theme',
