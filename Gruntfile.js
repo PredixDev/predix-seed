d5ef1e48f0be2513e09700bf50068134a8a24660
Spratley, Jonnie <jonnie.spratley@ge.com>
2014-12-11 16:12:49 -0800

    added grunt-config for dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
 		tmp: '.tmp',
 		artifactory: {
 			host: 'https://devcloud.swcoe.ge.com',
-			repo: 'DSP',
+			repo: 'DSP-SNAPSHOT',
 			username: 'svc-dsp-reader',
 			password: '4wxKT8u8E2'
 		}
@@ -342,7 +342,7 @@ module.exports = function (grunt) {
 							group_id: 'com.ge.predix',
 							name: 'iidx',
 							ext: 'zip',
-							version: '3.0.0',
+							version: '3.0.0.dev',
 							path: '<%= config.bower %>/iids'
 						}
 
@@ -352,7 +352,7 @@ module.exports = function (grunt) {
 			release: {
 				options: {
 					url: 'https://devcloud.swcoe.ge.com',
-					repository: 'DSP',
+					repository: '<%= grunt.config.get("repo") %>',
 					username: 'svc-dsp-deploy',
 					password: 'm3yqLMHpBy',
 					publish: [
