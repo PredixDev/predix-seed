eb44ad5e34870ae50499728ca110d1915a94306f
mattmomont <matt.momont@ge.com>
2015-02-04 11:30:11 -0800

    Made tests pass (renamed myapp to predixApp)

--- public/scripts/services/spec/services-spec.js
+++ public/scripts/services/spec/services-spec.js
@@ -1,7 +1,7 @@
 /* jshint unused:false */
 define(['angular-mocks', 'app'], function(mocks, app) {
     'use strict';
-    beforeEach(module('myapp'));
+    beforeEach(module('predixApp'));
 
     describe('sample-service', function() {
         it('should ...', function() {
