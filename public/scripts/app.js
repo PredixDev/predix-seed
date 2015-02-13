8d4752440b97d4389f454d08bc9264e5297b1b50
mattmomont <matt.momont@ge.com>
2015-02-13 10:06:12 -0800

    For lauren and Sabith

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -70,7 +70,7 @@ define([
         };
 
         // Example UAA Configuration
-        $scope.site = 'https://predixuaa.ges-apps.ice.ge.com';  // The location of your UAA server. The /oauth/token routes will be added by predix.oauth.
+        $scope.site = 'https://predixuaa.grc-apps.svc.ice.ge.com';  // The location of your UAA server. The /oauth/token routes will be added by predix.oauth.
         $scope.clientId = 'app';                                // Your app id that you registered with Cloud Foundry.
         $scope.redirectUri = $location.absUrl();                // Where the UAA server should redirect the user on successful login. Typically, the last page the user was visiting.
 
