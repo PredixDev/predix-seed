955afdd12816175f25f5e9dcab567d9c40bc9b54
Matt <Matt.Momont@ge.com>
2015-03-06 11:44:59 -0800

    Sample controller for overriding datasource options

--- public/scripts/controllers/overridingctrls.js
+++ public/scripts/controllers/overridingctrls.js
@@ -3,11 +3,20 @@ define(['angular', 'sample-module'], function(angular, controllers) {
 
     controllers.controller('MattsCtrl', function ($scope, $controller) {
 
-        $scope.beforeRequest = function(datasourceOptions) {
-            console.log(datasourceOptions);
-            datasourceOptions.tags = ['tag1', 'tag2', 'tagz'];
-            return datasourceOptions;
-        };
+        //$scope.beforeRequest = function(datasourceOptions) {
+        //    console.log(datasourceOptions);
+        //    datasourceOptions.tags = $scope.tags;
+        //    return datasourceOptions;
+        //};
+
+        $scope.$on('px-dashboard-event', function(event, name, tags){
+            if(name !== 'abc'){
+                return;
+            }
+
+            $scope.datasource.options.tags = tags;
+            $scope.fetch($scope.datasource);
+        });
 
         // Initialize the super class and extend it.
         angular.extend(this, $controller('WidgetRendererController', {$scope: $scope}));
