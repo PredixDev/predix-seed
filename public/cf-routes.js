dc830dfa8531b04ed6f4991c97e8e1590efdcbd2
matt <matt.momont@ge.com>
2015-03-16 12:11:42 -0700

    Removed getCfRoutes

--- public/cf-routes.js
+++ public/cf-routes.js
@@ -14,21 +14,6 @@
         }]
     };
 
-
-    window.getCfRoute = function (serviceName) {
-        for (var serviceId in VCAP_SERVICES) {
-            var service = VCAP_SERVICES[serviceId];
-            for (var instanceIndex in service) {
-                var instance = service[instanceIndex];
-                if (instance.name === serviceName) {
-                    return location.protocol + '//' + instance.credentials.url;
-                }
-            }
-        }
-        window.logger.error('Could not find a url for the service name ' + serviceName);
-        return serviceName;
-    };
-
     window.getRoutes = function () {
         var routes = {};
         for (var serviceId in VCAP_SERVICES) {
