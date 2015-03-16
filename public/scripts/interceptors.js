07e55f9308150f336d2ad01465c251c2dde7b41f
matt <matt.momont@ge.com>
2015-03-16 14:25:19 -0700

    Added 401 handler to redirect user on unauthorized

--- public/scripts/interceptors.js
+++ public/scripts/interceptors.js
@@ -3,13 +3,13 @@
  * This is the router definition that defines all application routes.
  */
 /*global define */
-define(['angular'], function(angular) {
+define(['angular'], function (angular) {
     'use strict';
     /**
      * Application configurations
      * This is where configuration is setup for your application.
      */
-    return angular.module('app.interceptors', []).config(['$httpProvider', function($httpProvider) {
+    return angular.module('app.interceptors', []).config(['$httpProvider', function ($httpProvider) {
 
         /*
          * Predix V server is expecting X-Requested-With header to identify the ajax traffic
@@ -22,26 +22,30 @@ define(['angular'], function(angular) {
          * If you are using Siteminder, this interceptor can be used to capture the session timeout on an AJAX request.
          * You can implement your conditions in this interceptor according to your own requirement.
          */
-        $httpProvider.interceptors.push(['$q', function($q) {
+        $httpProvider.interceptors.push(['$q', 'Endpoint', function ($q, Endpoint) {
             return {
                 // optional method
-                'request': function(config) {
+                'request': function (config) {
                     // do something before request
                     return config;
                 },
                 // optional method
-                'requestError': function(rejection) {
+                'requestError': function (rejection) {
                     //handle error
                     return $q.reject(rejection);
                 },
                 // optional method
-                'response': function(response) {
+                'response': function (response) {
                     // do something on success
                     return response;
                 },
-                // optional method
-                'responseError': function(rejection) {
-                    //handle error
+                // (optional) Redirect user to login page when unauthorized (401)
+                // If you want to allow 401's, you can remove this method.
+                'responseError': function (rejection) {
+                    if (rejection.status === 401) {
+                        Endpoint.redirect();
+                    }
+                    // handle error
                     return $q.reject(rejection);
                 }
             };
