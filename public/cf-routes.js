d57cc70a714f3e853f495298798050345087f483
Sabith KS <sabith.karippullil.soopy@ge.com>
2015-03-17 12:03:17 -0700

    Added predixAsset as a service

--- public/cf-routes.js
+++ public/cf-routes.js
@@ -1,30 +1,45 @@
 'use strict';
 /**
- *  Never ever include this file in the build, this file will be created automatically for you by the static build pack.
- */
+*  Never ever include this file in the build, this file will be created automatically for you by the static build pack.
+*/
 (function () {
     var VCAP_SERVICES = {
-        'View Service Free': [{
-            'credentials': {'url': 'dev-dashboard-server.grc-apps.svc.ice.ge.com'},
-            'label': 'View Service Free',
-            'name': 'viewPersistence',
-            'plan': 'Free View Service',
-            'syslog_drain_url': '',
-            'tags': []
-        }]
+        "View Service Free": [
+            {
+                "credentials": {
+                    "uri": "dev-dashboard-server.grc-apps.svc.ice.ge.com"
+                },
+                "label": "View Service Free",
+                "name": "viewPersistence",
+                "plan": "Free View Service",
+                "syslog_drain_url": "",
+                "tags": []
+            }
+        ],
+        "predix-asset": [
+            {
+                "credentials": {
+                    "uri": "predix-asset.grc-apps.svc.ice.ge.com"
+                },
+                "label": "predix-asset",
+                "name": "predixAsset",
+                "plan": "Basic",
+                "tags": []
+            }
+        ]
     };
 
-    window.getRoutes = function () {
-        var routes = {};
-        for (var serviceId in VCAP_SERVICES) {
-            var service = VCAP_SERVICES[serviceId];
-            for (var instanceIndex in service) {
-                var instance = service[instanceIndex];
-                routes[instance.name] = location.protocol + '//' + instance.credentials.url;
-            }
+window.getRoutes = function () {
+    var routes = {};
+    for (var serviceId in VCAP_SERVICES) {
+        var service = VCAP_SERVICES[serviceId];
+        for (var instanceIndex in service) {
+            var instance = service[instanceIndex];
+            routes[instance.name] = location.protocol + '//' + instance.credentials.uri;
         }
-        return routes;
-    };
+    }
+    return routes;
+};
 
 })();
 
