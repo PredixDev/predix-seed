168708c4a74761e874eaf1ff03e9a7815b5e3327
Chi, Clement (GE Global Research) <chi@ge.com>
2015-05-15 11:41:59 -0700

    change karma back to PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -78,7 +78,7 @@ module.exports = function(config) {
         // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
         // - PhantomJS
         // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
         captureTimeout: 60000,
 
 
