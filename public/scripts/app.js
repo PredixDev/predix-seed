ab11622ee93ab294f36eb430ea0d0e738c6ef7e1
Chi, Clement (GE Global Research) <chi@ge.com>
2015-05-28 10:17:00 -0700

    listening to px-card-ready event to initialize the card instead of relying on the angular $viewContextLoaded event.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -42,7 +42,7 @@ define([
      * This controller is the top most level controller that allows for all
      * child controllers to access properties defined on the $rootScope.
      */
-    predixApp.controller('MainCtrl', ['$scope', '$rootScope', '$location', '$timeout', function ($scope, $rootScope, $location, $timeout) {
+    predixApp.controller('MainCtrl', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
 
         //Global application object
         window.App = $rootScope.App = {
@@ -64,12 +64,6 @@ define([
         $scope.site = 'https://uaa-staging.nurego.com';  // The location of your UAA server. The /oauth/token routes will be added by predix.oauth.
         $scope.clientId = 'experience_seed_app';         // Your app id that you registered with Cloud Foundry.
         $scope.redirectUri = $location.absUrl();         // Where the UAA server should redirect the user on successful login. Typically, the last page the user was visiting.
-
-        $rootScope.$on('$viewContentLoaded', function () {
-            $timeout(function(){
-                document.querySelector('px-deck').init();
-            }, 10);
-        });
     }]);
 
 
