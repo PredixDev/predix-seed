e7c11cd069de69fd138324d05b858841f48aba8e
Chi, Clement (GE Global Research) <chi@ge.com>
2015-05-29 11:35:50 -0700

    loading px-deck views in dashboard page dynamically.

--- public/scripts/controllers/about.js
+++ public/scripts/controllers/about.js
@@ -1,5 +1,7 @@
 define(['angular', 'sample-module'], function(angular, sampleModule) {
     'use strict';
-    return sampleModule.controller('SampleCardsCtrl', [function() {
+    return sampleModule.controller('AboutCtrl', ['$scope', '$rootScope', '$log', function($scope, $rootScope, $log) {
+
+        $log.info('about controller loaded!');
     }]);
 });
--- public/scripts/controllers/sample-cards.js
+++ public/scripts/controllers/sample-cards.js
@@ -1,6 +1,6 @@
 define(['angular', 'sample-module'], function(angular, sampleModule) {
     'use strict';
-    return sampleModule.controller('CardToCardCtrl', ['$scope', function($scope) {
+    return sampleModule.controller('SampleCardsCtrl', ['$scope', function($scope) {
 
         $scope.context = {
             name: 'This is context'
