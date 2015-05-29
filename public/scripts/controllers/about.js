08730e498d230f11d65eae1c9e2212c28f1ce3fa
Lauren <lauren.bridge@ge.com>
2015-05-29 14:45:43 -0700

    fixing test

--- public/scripts/controllers/about.js
+++ public/scripts/controllers/about.js
@@ -2,6 +2,8 @@ define(['angular', 'sample-module'], function(angular, sampleModule) {
     'use strict';
     return sampleModule.controller('AboutCtrl', ['$scope', '$rootScope', '$log', function($scope, $rootScope, $log) {
 
+        $scope.name = 'About';
+
         $log.info('about controller loaded!');
     }]);
 });
