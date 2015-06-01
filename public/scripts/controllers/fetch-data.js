b579d5ce05543ec2cab37da091de206379c0aade
Lauren <lauren.bridge@ge.com>
2015-06-01 14:27:38 -0700

    show, hide, and todo for add

--- public/scripts/controllers/fetch-data.js
+++ public/scripts/controllers/fetch-data.js
@@ -1,7 +1,14 @@
 define(['angular', 'sample-module'], function(angular, sampleModule) {
     'use strict';
-    return sampleModule.controller('FetchData', [function() {
+    return sampleModule.controller('FetchData', ['$scope', function($scope) {
 
+        $scope.show = function() {
+            angular.element('#mycard')[0].showCard();
+        };
+
+        $scope.hide = function() {
+            angular.element('#mycard')[0].hideCard();
+        };
 
     }]);
 });
