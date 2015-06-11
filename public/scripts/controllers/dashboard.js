05480275c37d7e1ce809ab6ddfbe727f2a6f8b21
Lauren <lauren.bridge@ge.com>
2015-06-11 10:39:14 -0700

    adding /asset to custom baseurl since asset doesn't fix that anymore

--- public/scripts/controllers/dashboard.js
+++ public/scripts/controllers/dashboard.js
@@ -36,7 +36,7 @@ define(['angular',
 
         $scope.contextSelectorConfig = {
             //baseUrl: VCAP_SERVICES.predixAssetExp2 + '/services', // the base uri where your asset instance is
-            baseUrl: 'http://predix-asset-mvp2-exp1.grc-apps.svc.ice.ge.com/api', // the base uri where your asset instance is
+            baseUrl: 'http://predix-asset-mvp2-exp1.grc-apps.svc.ice.ge.com/api/asset', // the base uri where your asset instance is
             rootEntityId: null, // the root of the context browser
             onOpenContext: function(contextDetails) { // callback when the open button is hit in the context browser
                 $scope.$apply(function() {
