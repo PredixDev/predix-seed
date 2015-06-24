302eab65a0bdc43913c704ac8855628643d1c935
Chi, Clement (GE Global Research) <chi@ge.com>
2015-06-24 16:25:18 -0700

    removed oauth

--- public/scripts/interceptors.js
+++ public/scripts/interceptors.js
@@ -23,7 +23,7 @@ define(['angular'], function (angular) {
          * If you are using Siteminder, this interceptor can be used to capture the session timeout on an AJAX request.
          * You can implement your conditions in this interceptor according to your own requirement.
          */
-        $httpProvider.interceptors.push(['$q', 'LogoutService', function ($q, LogoutService) {
+        $httpProvider.interceptors.push(['$q', function ($q) {
             return {
                 // optional method
                 'request': function (config) {
@@ -43,9 +43,6 @@ define(['angular'], function (angular) {
                 // (optional) Redirect user to login page when unauthorized (401)
                 // If you want to allow 401's, you can remove this method.
                 'responseError': function (rejection) {
-                    if (rejection.status === 401) {
-                        LogoutService.hardLogout();
-                    }
                     // handle error
                     return $q.reject(rejection);
                 }
