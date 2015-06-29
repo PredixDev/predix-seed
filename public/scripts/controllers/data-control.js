624d90956a97fca987ed52a008d0aa3c312165e2
Eric Knudtson <knudtson@ge.com>
2015-06-29 14:28:45 -0700

    weather underground url for api call

--- public/scripts/controllers/data-control.js
+++ public/scripts/controllers/data-control.js
@@ -1,10 +1,12 @@
 define(['angular', 'sample-module'], function(angular, sampleModule) {
-    'use strict';
-    return sampleModule.controller('DataControlCtrl', ['$scope', function($scope) {
+  'use strict';
+  return sampleModule.controller('DataControlCtrl', ['$scope', function($scope) {
 
-        $scope.context = {
-            name: 'This is context'
-        };
+    $scope.context = {
+      name: 'This is context',
+      // using api from weather underground: http://www.wunderground.com/
+      url: 'http://api.wunderground.com/api/e77862ea276e303e/conditions/q/CA/San_Francisco.json?callback=JSON_CALLBACK'
+    };
 
-    }]);
+  }]);
 });
