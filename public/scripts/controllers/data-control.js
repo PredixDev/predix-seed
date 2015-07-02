2e3e8029a38dc07ace9d1a8534f3186d5af4f65a
Eric Knudtson <knudtson@ge.com>
2015-07-02 14:44:36 -0700

    fixing ' quotes and indention

--- public/scripts/controllers/data-control.js
+++ public/scripts/controllers/data-control.js
@@ -3,30 +3,30 @@ define(['angular', 'sample-module'], function (angular, sampleModule) {
     return sampleModule.controller('DataControlCtrl', ['$scope', function ($scope) {
 
         $scope.context = {
-            name: "This is context",
+            name: 'This is context',
             // using api from weather underground: http://www.wunderground.com/
-            url: "http://api.wunderground.com/api/e77862ea276e303e/conditions/q/CA/San_Ramon.json?callback=JSON_CALLBACK",
+            url: 'http://api.wunderground.com/api/e77862ea276e303e/conditions/q/CA/San_Ramon.json?callback=JSON_CALLBACK',
             urls: [
-              {
-                name: "San Ramon",
-                url: "http://api.wunderground.com/api/e77862ea276e303e/conditions/q/CA/San_Ramon.json?callback=JSON_CALLBACK"
-              },
-              {
-                name: "Niskayuna",
-                url: "http://api.wunderground.com/api/e77862ea276e303e/conditions/q/NY/Niskayuna.json?callback=JSON_CALLBACK"
-              },
-              {
-                name: "Rio de Janeiro",
-                url: "http://api.wunderground.com/api/e77862ea276e303e/conditions/q/Brazil/Rio_de_Janeiro.json?callback=JSON_CALLBACK"
-              },
-              {
-                name: "Munich",
-                url: "http://api.wunderground.com/api/e77862ea276e303e/conditions/q/Germany/Munich.json?callback=JSON_CALLBACK"
-              },
-              {
-                name: "Shanghai",
-                url: "http://api.wunderground.com/api/e77862ea276e303e/conditions/q/China/Shanghai.json?callback=JSON_CALLBACK"
-              }
+                {
+                    name: 'San Ramon',
+                    url: 'http://api.wunderground.com/api/e77862ea276e303e/conditions/q/CA/San_Ramon.json?callback=JSON_CALLBACK'
+                },
+                {
+                    name: 'Niskayuna',
+                    url: 'http://api.wunderground.com/api/e77862ea276e303e/conditions/q/NY/Niskayuna.json?callback=JSON_CALLBACK'
+                },
+                {
+                    name: 'Rio de Janeiro',
+                    url: 'http://api.wunderground.com/api/e77862ea276e303e/conditions/q/Brazil/Rio_de_Janeiro.json?callback=JSON_CALLBACK'
+                },
+                {
+                    name: 'Munich',
+                    url: 'http://api.wunderground.com/api/e77862ea276e303e/conditions/q/Germany/Munich.json?callback=JSON_CALLBACK'
+                },
+                {
+                    name: 'Shanghai',
+                    url: 'http://api.wunderground.com/api/e77862ea276e303e/conditions/q/China/Shanghai.json?callback=JSON_CALLBACK'
+                }
             ]
         };
 
