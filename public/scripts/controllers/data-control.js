e6e2bab47658a18297ed3c7ec766777a207850e2
Eric Knudtson <knudtson@ge.com>
2015-07-02 14:28:54 -0700

    san ramon - default city

--- public/scripts/controllers/data-control.js
+++ public/scripts/controllers/data-control.js
@@ -3,9 +3,9 @@ define(['angular', 'sample-module'], function (angular, sampleModule) {
     return sampleModule.controller('DataControlCtrl', ['$scope', function ($scope) {
 
         $scope.context = {
-            name: 'This is context',
+            name: "This is context",
             // using api from weather underground: http://www.wunderground.com/
-            url: 'http://api.wunderground.com/api/e77862ea276e303e/conditions/q/CA/San_Francisco.json?callback=JSON_CALLBACK',
+            url: "http://api.wunderground.com/api/e77862ea276e303e/conditions/q/CA/San_Ramon.json?callback=JSON_CALLBACK",
             urls: [
               {
                 name: "San Ramon",
