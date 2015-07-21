596a3c104435426a4bb3ec192a46335270a3ca59
Chi, Clement (GE Global Research) <chi@ge.com>
2015-07-21 10:40:46 -0700

    use px.auth in angular service

--- public/scripts/services/predix-user-service.js
+++ public/scripts/services/predix-user-service.js
@@ -4,19 +4,22 @@ define(['angular', 'sample-module'], function (angular, module) {
     /**
      * PredixAssetService is a sample angular service that integrates with Predix Asset Server API
      */
-    module.factory('PredixUserService', ['$q', '$http', '$location', function ($q, $http, $location) {
+    module.factory('PredixUserService', ['$q', function ($q) {
         return {
             isAuthenticated: function(){
                 return this.getUserInfo();
             },
             login: function(uiState){
-                var protocol = $location.protocol();
-                var host = $location.host();
-                var port = $location.port();
-                window.location.replace('/login?redirectUri='+protocol+'://'+host+':'+port+uiState.url);
+                window.px.auth.login(uiState);
             },
             getUserInfo: function(){
-                return $http.get('/userinfo');
+                var deferred = $q.defer();
+                window.px.auth.getUserInfo().then(function(){
+                    deferred.resolve();
+                }, function(){
+                    deferred.reject();
+                });
+                return deferred.promise;
             }
         };
     }]);
