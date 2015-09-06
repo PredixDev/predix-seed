1afcd3099425fe0ac8d186d0e99bdc23ae9d5418
Chi, Clement (GE Global Research) <chi@ge.com>
2015-09-05 17:40:43 -0700

    1st working version

---
+++ public/scripts/services/main.js
@@ -0,0 +1,6 @@
+/*
+ * load angular app.services files - angular.module("app.services");
+ */
+define(['./sample-service','./predix-asset-service', './predix-user-service', './predix-view-service'], function() {
+
+});
