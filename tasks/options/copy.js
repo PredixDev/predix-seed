8959448ed76fa8f9caf398da343074f09449d9aa
Lauren <lauren.bridge@ge.com>
2015-09-10 09:43:27 -0700

    Dist working except for dashboard and bug with app nav.

--- tasks/options/copy.js
+++ tasks/options/copy.js
@@ -15,7 +15,9 @@ module.exports = {
                     'views/*',
                     'bower_components/px/dist/px.min.js',
                     'bower_components/es6-promise/dist/es6-promise.min.js',
-                    'bower_components/requirejs/require.js'
+                    'bower_components/requirejs/require.js',
+                    'bower_components/font-awesome/fonts/*',
+                    'bower_components/px-typography-design/type/*'
                 ],
                 dest: 'dist/www/'
             }
