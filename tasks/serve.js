d9dd9beb2033b924a08f4ff6fc82f2fd55f12fa5
Chi, Clement (GE Global Research) <chi@ge.com>
2015-09-11 22:01:28 -0700

    finish refactor grunt connect.js

--- tasks/serve.js
+++ tasks/serve.js
@@ -11,5 +11,5 @@
  *
  */
 module.exports = function(grunt) {
-    grunt.registerTask('serve', ['cssmin', 'clean:build', 'connect:development', 'watch']);
+    grunt.registerTask('serve', ['cssmin', 'clean:build', 'connect:server', 'watch']);
 }
