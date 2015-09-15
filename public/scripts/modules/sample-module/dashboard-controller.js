eb4d4f89290cc1efe97014464c617e111221e13f
Chi, Clement (GE Global Research) <chi@ge.com>
2015-09-15 13:26:40 -0700

    view service integrated, WIP to integrate asset

--- public/scripts/modules/sample-module/dashboard-controller.js
+++ public/scripts/modules/sample-module/dashboard-controller.js
@@ -1,44 +1,54 @@
-define(['angular', './sample-module'], function(angular, controllers) {
-    'use strict';
+define(['angular', './sample-module'], function (angular, controllers) {
+  'use strict';
 
-    // Controller definition
-    controllers.controller('DashboardsCtrl', ['$scope', '$log', 'PredixAssetService', 'PredixViewService', function ($scope, $log, PredixAssetService, PredixViewService) {
+  // Controller definition
+  controllers.controller('DashboardsCtrl', ['$scope', '$log', 'PredixAssetService', 'PredixViewService', function ($scope, $log, PredixAssetService, PredixViewService) {
 
-        PredixAssetService.getAssetsByParentId(null).then(function(initialContext) {
-            $scope.initialContexts = initialContext;
-        }, function(message) {
-            $log.error(message);
+    PredixAssetService.getAssetsByParentId(null).then(function (initialContext) {
+      $scope.initialContexts = initialContext;
+    }, function (message) {
+      $log.error(message);
+    });
+
+//    $scope.decks = [{
+//        name: 'Sample Deck',
+//        url: '/api/views/decks/1/cards'
+//    }];
+//
+//    $scope.selectedDeck = $scope.decks[0].url;
+
+    // callback for when the Open button is clicked
+    $scope.openContext = function (contextDetails) {
+
+      // need to clean up the context details so it doesn't have the infinite parent/children cycle,
+      // which causes problems later (can't interpolate: {{context}} TypeError: Converting circular structure to JSON)
+      var newContext = angular.copy(contextDetails);
+      newContext.children = [];
+      newContext.parent = [];
+
+      $scope.context = newContext;
+
+      console.log(newContext);
+
+      //Tag string can be classification from contextDetails
+      PredixViewService.getDecksByTags()
+        .then(function (decks) {
+          decks.forEach(function (deck) {
+            $scope.decks.push({name: deck.title, url: '/api/decks/' + deck.id});
+          });
         });
 
-        // callback for when the Open button is clicked
-        $scope.openContext = function(contextDetails) {
+      $scope.$digest();
+    };
 
-            // need to clean up the context details so it doesn't have the infinite parent/children cycle,
-            // which causes problems later (can't interpolate: {{context}} TypeError: Converting circular structure to JSON)
-            var newContext = angular.copy(contextDetails);
-            newContext.children = [];
-            newContext.parent = [];
+    $scope.getChildren = function (parentId, options) {
+      return PredixAssetService.getAssetsByParentId(parentId, options);
+    };
 
-            $scope.context = newContext;
-
-            $scope.selectedDeck = 'bower_components/px-sample-cards/sample-deck.html';
-
-            $scope.$digest();
-        };
-
-        $scope.getChildren = function(parentId, options) {
-            return PredixAssetService.getAssetsByParentId(parentId, options);
-        };
-
-        $scope.handlers = {
-            itemOpenHandler: $scope.openContext,
-            getChildren: $scope.getChildren
-            // (optional) click handler: itemClickHandler: $scope.clickHandler
-        };
-
-        PredixViewService.getDecksByTags('Contra Costa')
-            .then(function(res){
-                console.log(res);
-            });
-    }]);
+    $scope.handlers = {
+      itemOpenHandler: $scope.openContext,
+      getChildren: $scope.getChildren
+      // (optional) click handler: itemClickHandler: $scope.clickHandler
+    };
+  }]);
 });
