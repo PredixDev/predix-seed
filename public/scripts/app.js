6f1ae3a746256c0b0f7696fb69dc63ff1613acf0
Eric Knudtson <ericknudtson@gmail.com>
2016-02-24 12:22:21 -0800

    checkpoint

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,40 +8,33 @@ app.global = {
   session: {}
 };
 
-app.tabs = [
-  {
-    'label': 'Alerts',
-    'path': '',
-    'icon': 'fa-warning',
-    'subitems': [
-      {
-        'label': 'Sub Category 1'
-      }, {
-        'label': 'Sub Category 2'
-      }
-    ]
-  },
-  {
-    'label': 'Cases',
-    'path': 'tab2',
-    'icon': 'fa-briefcase'
-  },
-  {
-    'label': 'Angular',
-    'path': 'angular',
-    'icon': 'fa-briefcase'
-  }
-];
+app.tabs = [{
+  'label': 'Dashboards',
+  'path': 'dashboards',
+  'icon': 'fa-tachometer'
+}, {
+  'label': 'Blank Page',
+  'path': 'blankpage',
+  'icon': 'fa-file-o',
+  'subitems': [{
+    'label': 'Blank Sub Page',
+    'path': 'blanksubpage'
+  }]
+}, {
+  'label': 'Angular',
+  'path': 'angular',
+  'icon': 'fa-briefcase'
+}];
 
 // Sets app default base URL
 app.baseUrl = '/';
 
 // Listen for dom change and set app.tabs
 app.addEventListener('dom-change', function() {
-    // console.log('dom-change event');
+  // console.log('dom-change event');
 });
 
 // listen for WebComponentsReady event and set flag
 window.addEventListener('WebComponentsReady', function() {
-    app.webComponentsReady = true;
+  app.webComponentsReady = true;
 });
