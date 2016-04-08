f2eaabcb8f57936f62eeeec982330dd54e487574
Greg Stroup <gregory.stroup@ge.com>
2016-04-07 17:03:13 -0700

    put internal dev info back into configuration.  add comments

--- tasks/options/connect.js
+++ tasks/options/connect.js
@@ -7,12 +7,13 @@ var config = {
    *
    * This uaa helper object simulates NGINX uaa integration using Grunt allowing secure cloudfoundry service integration in local development without deploying your application to cloudfoundry.
    * Please update the following uaa configuration for your solution
+   * You'll need to update clientId, serverUrl, and base64ClientCredential.
    */
   uaa: {
-    clientId: 'your-app-client-id',
-    serverUrl: 'https://your-uaa-instance-id.predix-uaa.run.aws-usw02-pr.ice.predix.io',
+    clientId: 'predix-seed',
+    serverUrl: 'https://etc.predix-uaa-staging.grc-apps.svc.ice.ge.com',
     defaultClientRoute: '/about',
-    base64ClientCredential: 'your-base64-encoded-client-id:client-secret'
+    base64ClientCredential: 'cHJlZGl4LXNlZWQ6TTBhVzdrTmZRRndyTTZ3ZHJpV2h3bVc2ck1HQ045Q0x1cnI5VnI3elc0cz0='
   },
   /**
    * --------- ADD YOUR SECURE ROUTES HERE ------------
@@ -20,12 +21,12 @@ var config = {
    * Please update the following object add your secure routes
    *
    * Note: Keep the /api in front of your services here to tell the proxy to add authorization headers.
+   * You'll need to update the url and instanceId.
    */
   proxy: {
-    '/api/view-service': {
-      url: 'https://predix-views.run.aws-usw02-pr.ice.predix.io',
-      instanceId: 'your-views-service-instance-guid',
-      pathRewrite: { '^/api/view-service': '/v1'}
+    '/api/view-service(.*)': {
+      url: 'http://predix-views-dev.grc-apps.svc.ice.ge.com/v1$1',
+      instanceId: '49a92fd6-df7b-45f6-925e-0bca94be7313'
     }
   }
 };
