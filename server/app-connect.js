951946af44228146fa1fffe4da874e58d4f5b549
gedesigntech <henry.navarro@ge.com>
2016-04-21 16:46:18 -0700

    connect/http(1.1) implementation of view-service & asset middleware

--- server/app-connect.js
+++ server/app-connect.js
@@ -1,17 +1,9 @@
-/*
- * Implementation of the asset service backend for the polymer-based seed
- * using the Connect (https://github.com/senchalabs/connect) HTTP middleware
- * framework
-*/
-
 var connect = require('connect');
 var http = require('http');
-var http2 = require('http2');
-
-var app = connect();
-
 var fs = require("fs");
 var _ = require("lodash");
+
+var app = connect();
 
 // allow CORS
 app.use(function(req, res, next) {
@@ -22,11 +14,11 @@ app.use(function(req, res, next) {
 });
 
 // parse
-var getPathFromParams = function(params) {
+var getPathFromParams = function(params, pathPrefix) {
   // parse out values & remove undefined
   var pathValues = _.pull(_.values(params), undefined);
   var i = 0,
-    pathString = __dirname + "/predix-asset-sample-data";
+    pathString = __dirname + pathPrefix;
   // iterate over values and concat path string
   for (i; i < pathValues.length; i++) {
     pathString += '/';
@@ -37,57 +29,57 @@ var getPathFromParams = function(params) {
   return pathString;
 };
 
-app.use(function(req, res) {
-  // res.end('Hello from the polymer-seed asset server!');
-  var pathString = '';
-  if ('string' === typeof req.url) {
-    if (req.url.match(/^(?:\/api){1}(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?$/)) {
-      // remove "/api" from the processing of path sections
-      var urlString = req.url.substring(4);
-      // fetch the url path sections ('/'-separated)
-      // and store in req.params
-      req.params = urlString.split('/');
-      // split() above adds unwanted empty first element that
-      // needs to be shifted out below
-      req.params.shift();
-      pathString = getPathFromParams(req.params);
+// get '/predix-asset/uri'
+// examples: '/predix-asset/root', 'api/enterprises/san-ramon-oil'
+var assetRoute =
+  /^(?:\/api\/predix-asset){1}(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?$/,
+  viewServiceRoute =
+  /^(?:\/api\/view-service\/){1}(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?$/;
 
-      // Fetch the file corresponding to the derived path
-      fs.readFile( pathString, 'utf8', function (err, data) {
-        if (err) {
-          if (err.code !== 'ENOENT') {
-            throw err;
-          }
-          else {
-            res.end('Asset file not found');
-          }
-        }
-        else {
-          obj = JSON.parse(data);
-          res.end(JSON.stringify (obj));
-        }
-      });
+app.use(function(req, res) {
+  var pathString = '', pathPrefix = '';
+  if ('string' === typeof req.url) {
+    if (req.url.match(assetRoute)) {
+      pathPrefix = "/predix-asset-sample-data";
     }
     else {
-      res.end('Invalid path string');
+      if (req.url.match(viewServiceRoute)) {
+        pathPrefix = "/view-service-sample-data";
+      }
+      else {
+        res.end('Invalid path');
+      }
     }
+
+    // remove "/api" from the processing of path sections
+    var urlString = req.url.substring(17);
+    // fetch the url path sections ('/'-separated)
+    // and store in req.params
+    req.params = urlString.split('/');
+    // split() above adds unwanted empty first element that
+    // needs to be shifted out below
+    req.params.shift();
+    pathString = getPathFromParams(req.params, pathPrefix);
+
+    // Fetch the file corresponding to the derived path
+    fs.readFile( pathString, 'utf8', function (err, data) {
+      if (err) {
+        if (err.code !== 'ENOENT') {
+          throw err;
+        }
+        else {
+          res.end('Asset file not found');
+        }
+      }
+      else {
+        obj = JSON.parse(data);
+        res.end(JSON.stringify (obj));
+      }
+    });
   }
   else {
     res.end('Invalid request URL type');
   }
 });
 
-var options = {
-  key: fs.readFileSync( __dirname + '/http2_keyfile.key' ),
-  cert: fs.readFileSync( __dirname + '/http2_certfile.crt' )
-};
-
-http2.createServer(options, app).listen(process.env.VCAP_APP_PORT || 8181);
-
-// http.createServer(app).listen(process.env.VCAP_APP_PORT || 8181);
-
-/*
-var server = app.listen(8181, function() {
-  console.log('API mock app listening on port 8181.');
-});
-*/
+http.createServer(app).listen(process.env.VCAP_APP_PORT || 8181);
