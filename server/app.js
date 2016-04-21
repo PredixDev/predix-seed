76396f788cea09ea62ba144fd10e273a18178844
Eric Knudtson <ericknudtson@gmail.com>
2016-04-21 00:00:51 -0700

    change api mock route for predix-asset data and add new routes for view-service

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
 });
 
 // parse
-var getPathFromParams = function(params) {
+var getAssetPathFromParams = function(params) {
   // parse out values & remove undefined
   var pathValues = _.pull(_.values(params), undefined);
   var i = 0,
@@ -31,11 +31,42 @@ var getPathFromParams = function(params) {
   return pathString;
 };
 
-// get '/api/uri'
-// examples: '/api/root', 'api/enterprises/san-ramon-oil'
-app.get(/^(?:\/api){1}(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?$/, function(req, res) {
-  var pathString = getPathFromParams(req.params);
-  console.log(pathString);
+// get '/predix-asset/uri'
+// examples: '/predix-asset/root', 'api/enterprises/san-ramon-oil'
+var assetRoute =
+  /^(?:\/api\/predix-asset){1}(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?$/;
+app.get(assetRoute, function(req, res) {
+  var pathString = getAssetPathFromParams(req.params);
+  fs.readFile(pathString, 'utf8', function(err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+    res.send(JSON.stringify(obj));
+  });
+});
+
+
+// parse
+var getViewServicePathFromParams = function(params) {
+  // parse out values & remove undefined
+  var pathValues = _.pull(_.values(params), undefined);
+  var i = 0,
+    pathString = __dirname + "/view-service-sample-data";
+  // iterate over values and concat path string
+  for (i; i < pathValues.length; i++) {
+    pathString += '/';
+    pathString += pathValues[i];
+  };
+  // concat .json file string
+  pathString += '/' + _.last(pathValues) + '.json';
+  return pathString;
+};
+
+// view service routes
+// example: /api/view-service/decks/1/cards/1
+var viewServiceRoute =
+  /^(?:\/api\/view-service\/){1}(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?(?:\/)?([\w\d-]+)?$/;
+app.get(viewServiceRoute, function(req, res) {
+  var pathString = getViewServicePathFromParams(req.params);
   fs.readFile(pathString, 'utf8', function(err, data) {
     if (err) throw err;
     obj = JSON.parse(data);
