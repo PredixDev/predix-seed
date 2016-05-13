e4c9bb1a359f33e4bd033557797ccc546ee39e5b
Eric Knudtson <ericknudtson@gmail.com>
2016-05-12 17:06:17 -0700

    removing unnecessary files

--- public/elements/seed-app/test/seed-app-custom-tests.js
+++
@@ -1,9 +0,0 @@
-// This is the wrapper for custom tests, called upon web components ready state
-function runCustomTests() {
-  // Place any setup steps like variable declaration and initialization here
-
-  // This is the placeholder suite to place custom tests in
-  // Use testCase(options) for a more convenient setup of the test cases
-  suite('Custom Automation Tests for seed-app', function() {
-  });
-};
