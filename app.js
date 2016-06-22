8c0ae7cc5bf5ed47c9cd75ee2e2fb6212e3ca6ed
Eric Knudtson <ericknudtson@gmail.com>
2016-06-21 21:38:02 -0700

    add time-series routes, cleanup timeseries data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var _ = require('lodash');
 var server = jsonServer.create();
 var viewServiceRoutes = require('./server/view-service-routes.js')();
 var assetRoutes = require('./server/predix-asset-routes.js')();
+var timeSeriesRoutes = require('./server/time-series-routes.js')();
 
 // Set default middlewares (logger, static '/public', CORS and no-cache)
 var defaultMiddlewares = jsonServer.defaults();
@@ -15,6 +16,10 @@ server.use('/api/view-service', viewServiceAPI);
 // add predix asset api routes
 var assetAPI = jsonServer.router(assetRoutes);
 server.use('/api/predix-asset', assetAPI);
+
+// add time-series api routes
+var timeSeriesAPI = jsonServer.router(timeSeriesRoutes);
+server.use('/api/time-series', timeSeriesAPI);
 
 // open up port (Cloud Foundry setting or 5000)
 var expressPort = process.env.VCAP_APP_PORT || 5000;
