a77e9b4d5a13277b95ed8873a6790d5d7eb3f428
Eric Knudtson <ericknudtson@gmail.com>
2016-07-13 13:57:10 -0700

    remove extraneous routes config

--- public/routes.js
+++
@@ -1,8 +0,0 @@
-// use addon to mock http request
-module.exports = {
-  // GET
-  "GET /home": "index.html",
-  "GET /dashboards": "index.html",
-  "GET /blank-page": "index.html",
-  "GET /blank-sub-page": "index.html"
-}
