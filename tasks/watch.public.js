1e9f9ff164243c6182f82a3723052cf1018039da
Korbin Bickel <leebickmtu@users.noreply.github.com>
2016-11-24 14:15:20 -0500

    Fix public path.

--- tasks/watch.public.js
+++ tasks/watch.public.js
@@ -7,6 +7,6 @@
 module.exports = function(gulp) {
   return function() {
     gulp.watch('./public/**/*.scss', ['compile:sass']);
-    gulp.watch(['./public/_index.html','./public/_index-inline-loading-script.js','index-inline.css'], ['compile:index']);
+    gulp.watch(['./public/_index.html','./public/_index-inline-loading-script.js','./public/index-inline.css'], ['compile:index']);
   };
 };
