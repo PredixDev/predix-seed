7245fcd3fb07ed2e208f27d4bf07c8126c288465
Greg Stroup <gregory.stroup@ge.com>
2016-12-02 09:34:58 -0800

    only set up secure routes if UAA is configured, else serve up everything.

--- server/app.js
+++ server/app.js
@@ -69,8 +69,6 @@ var server = app.listen(process.env.VCAP_APP_PORT || 5000, function () {
 	console.log ('Server started on port: ' + server.address().port);
 });
 
-// app.use(express.static(path.join(__dirname, process.env['base-dir'] ? process.env['base-dir'] : '../public')));
-
 /*******************************************************
 SET UP MOCK API ROUTES
 *******************************************************/
@@ -88,7 +86,9 @@ app.use('/api/time-series', jsonServer.router(timeSeriesRoutes));
 	SET UP EXPRESS ROUTES
 *****************************************************************************/
 
-if (uaaIsConfigured) {
+if (!uaaIsConfigured) { // no restrictions
+  app.use(express.static(path.join(__dirname, process.env['base-dir'] ? process.env['base-dir'] : '../public')));
+} else {
   //login route redirect to predix uaa login page
   app.get('/login',passport.authenticate('predix', {'scope': ''}), function(req, res) {
     // The request will be redirected to Predix for authentication, so this
@@ -111,15 +111,7 @@ if (uaaIsConfigured) {
   	res.redirect('/');
     });
 
-  //secure route checks for authentication
-  app.get('/secure', passport.authenticate('main', {
-  	noredirect: true //Don't redirect a user to the authentication page, just show an error
-    }), function(req, res) {
-  	console.log('Accessing the secure route');
-    // modify this to send a secure.html file if desired.
-  	res.send('<h2>This is a sample secure route.</h2>');
-  });
-
+  // example of calling a custom microservice.
   if (windServiceURL && windServiceURL.indexOf('https') === 0) {
     app.get('/windy/*', passport.authenticate('main', { noredirect: true}),
       // if calling a secure microservice, you can use this middleware to add a client token.
@@ -127,6 +119,24 @@ if (uaaIsConfigured) {
       proxy.customProxyMiddleware('/windy', windServiceURL)
     );
   }
+
+  //Use this route to make the entire app secure.  This forces login for any path in the entire app.
+  app.use('/', passport.authenticate('main', {
+    noredirect: false //Don't redirect a user to the authentication page, just show an error
+    }),
+    express.static(path.join(__dirname, process.env['base-dir'] ? process.env['base-dir'] : '../public'))
+  );
+  //Or remove the above route, and follow this pattern to create secure routes,
+  // if only some portions of the app are secure.
+  /*
+  app.get('/secure', passport.authenticate('main', {
+    noredirect: true //Don't redirect a user to the authentication page, just show an error
+    }), function(req, res) {
+    console.log('Accessing the secure route');
+    // modify this to send a secure.html file if desired.
+    res.send('<h2>This is a sample secure route.</h2>');
+  });
+  */
 }
 
 //logout route
@@ -140,13 +150,6 @@ app.get('/logout', function(req, res) {
 app.get('/favicon.ico', function (req, res) {
 	res.send('favicon.ico');
 });
-
-//secure route checks for authentication
-app.use('/', passport.authenticate('main', {
-  noredirect: false //Don't redirect a user to the authentication page, just show an error
-  }),
-  express.static(path.join(__dirname, process.env['base-dir'] ? process.env['base-dir'] : '../public'))
-);
 
 // Sample route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
