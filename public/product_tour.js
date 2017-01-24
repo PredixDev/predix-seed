4ff1685cf2c8e7a80f16210f225a751cc5a8c236
gedesigntech <henry.navarro@ge.com>
2017-01-24 14:41:28 -0800

    temporary codebase for progress coaching feature

---
+++ public/product_tour.js
@@ -0,0 +1,97 @@
+window.hopscotchTour = {
+  'tour1' : {
+    id: "basic-seed-features",
+    steps: [
+    {
+      title: "Basic Seed Application",
+      content: "Congratulations!  You have successfully installed a basic form of the Predix UI Seed, with essential features like header, navigation, content area and footer.",
+      target: "progress-item-1",
+      xOffset: -20,
+      placement: "bottom",
+    },
+    {
+      title: "Seed Header",
+      content: "Page header for the Predix UI Seed",
+      target: "seed-header",
+      placement: "bottom"
+    },
+    {
+      title: "App Navigation",
+      content: "Navigation using the 'px-app-nav' Predix UI Component",
+      target: "app-nav",
+      placement: "right",
+    },
+    {
+      title: "Content Area",
+      content: "Main content area",
+      target: "content-area",
+      xOffset: "center",
+      yOffset: "center",
+      placement: "top",
+    },
+    {
+      title: "Sample View",
+      content: "A Predix UI component for generic view encapsulation",
+      target: "sample-view",
+      xOffset: "center",
+      placement: "bottom",
+    },
+    {
+      title: "Footer",
+      content: "Application footer",
+      target: "seed-footer",
+      placement: "top",
+    }
+    ]
+  },
+  'tour2' : {
+    id: 'visualization-features',
+    steps: [
+    {
+      title: "Advanced Visual Components",
+      content: "Congratulations!  You have successfully installed advanced components like the Context Browser and Charts as part of enhanced visual features for the Predix UI Seed.",
+      target: "progress-item-2",
+      xOffset: -20,
+      placement: "bottom",
+    },
+    {
+      title: "Context Browser",
+      content: "A Predix UI component geared for asset tree representation",
+      target: "contextBrowser",
+      xOffset: "center",
+      placement: "bottom",
+    },
+    {
+      title: "Sample Visualizations",
+      content: "Example Predix UI visualization components",
+      target: "widgetsx3",
+      xOffset: 500,
+      placement: "top",
+    }
+    ]
+  },
+  'tour3' : {
+    id: 'data-integration',
+    steps: [
+    {
+      title: "Advanced Data Components",
+      content: "You are currently at a stage of the Predix UI Seed where you will be adding features for data integration, specifically with an instance of the Time Seriex Predix Service.",
+      target: "progress-item-3",
+      xOffset: -20,
+      placement: "bottom",
+    }
+  ]
+  },
+  'tour4' : {
+    id: 'analytics-features',
+    steps: [
+    {
+      title: "Advanced Analytics",
+      content: "Your next step is the installation of advanced analytics features.  Upon completion of your current step, these features will be enabled.",
+      target: "progress-item-4",
+      xOffset: -20,
+      placement: "bottom",
+    }
+  ]
+  }
+}
