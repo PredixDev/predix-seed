b231654ae9181a59e09e302b00a90c5f388060b6
Garry Yao <yaojun85@gmail.com>
2017-02-20 21:34:32 +0800

    optimize loader bundle size by 50%

--- tasks/dist.copy.js
+++ tasks/dist.copy.js
@@ -12,7 +12,9 @@ module.exports = function(gulp) {
       'public/images/*',
       'public/bower_components/px-typography-design/type/*',
       'public/bower_components/font-awesome/fonts/*',
-      'public/bower_components/px-demo-data/demo-data/**'
+      'public/bower_components/px-demo-data/demo-data/**',
+      // list of lazy-load modules, see also "compile.vulcanize.js"
+      'public/bower_components/{*d3,canvg,numbro,pxmoment-timezone}/**',
     ], {base: 'public'})
       .pipe(gulp.dest('./dist'));
   };
